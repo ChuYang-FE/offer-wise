66319bc06466cb0e4125c918f67276a12a6f3c47
William Tan <youngtam733@gmail.com>
2024-09-09 00:22:57 +0800

    chore: update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const a = [1, 2, 3];
+
+  const b = a.findLast((a) => a);
 
   return (
     <>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
