e8369cbd7aed1664ecf21453d254d7414588c11e
William Tan <youngtam733@gmail.com>
2024-09-12 19:08:12 +0800

    feat: update to @vitejs/plugin-react-swc

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       globals: globals.browser,
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      Hello Offer Wise!
+      <div className="bg-white p-4 rounded-md">
+        <h1 className="hover:bg-red-400 transition-colors rotate-6">
+          Hello Offer Wise!
+        </h1>
+      </div>
     </div>
   );
 }
