b66eed0c24aa09f6a66bea0f7a3f462e5c9b3db2
William Tan <youngtam733@gmail.com>
2024-09-14 23:57:06 +0800

    feat: adopt NextUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Button } from "@nextui-org/button";
+
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="rounded-md bg-white p-4">
-        <h1 className="my-2 size-full rotate-6 transition-colors hover:bg-red-400">
-          Hello Offer Wise!
-        </h1>
+    <div className="flex min-h-screen items-center justify-cente">
+      <div className="rounded-md p-4">
+        <h1 className="my-2 size-full">Hello Offer Wise!</h1>
       </div>
+      <Button>Press me</Button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <NextUIProvider>
+      <main className="dark text-foreground bg-background">
+        <App />
+      </main>
+    </NextUIProvider>
   </StrictMode>
 );
