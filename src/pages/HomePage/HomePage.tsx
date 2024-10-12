7eef4177807187322651ebf58206e0eca26ddb0a
William Tan <youngtam733@gmail.com>
2024-10-12 18:54:50 +0800

    feat: config auto import for react & react router dom

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import { Button, Pagination, Switch } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
   const navigate = useNavigate();
