d89238a7b08ea473071045afe3c63c6abe862392
William Tan <v-witan@expediagroup.com>
2024-10-12 14:54:50 +0800

    feat: config auto import for react & react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { NextUIProvider } from "@nextui-org/react";
-import { Route, Routes, useHref, useNavigate } from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  NextUIProvider,
+} from "@nextui-org/react";
 
 import { About, HomePage, NotFound } from "./pages";
 
 function App() {
   const navigate = useNavigate();
+  const [count, setCount] = useState(0);
 
   return (
     <NextUIProvider navigate={navigate} useHref={useHref}>
       <div className="flex min-h-screen flex-col bg-background text-foreground light">
-        <header className="flex w-full bg-slate-300 p-2">this is header</header>
+        <header className="flex w-full bg-slate-300 p-2">
+          <Card className="w-1/2">
+            <CardHeader>this is header</CardHeader>
+            <CardBody>
+              <Button onClick={() => setCount(count + 1)}>+1</Button>
+            </CardBody>
+            <CardFooter>click: {count}</CardFooter>
+          </Card>
+        </header>
         <main className="flex grow flex-col bg-slate-400 p-2">
           <section className="bg-pink-200">
             <div>Section 1</div>
--- src/components/common/GoToHomeButton.tsx
+++ src/components/common/GoToHomeButton.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
 
 export const GoToHomeButton = () => {
   const navigate = useNavigate();
